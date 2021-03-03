4a898951b1243e7a9770505b016e3194fca14252
Alexander Köpke <koepalex@xdigital.de>
2021-03-04 00:31:13 +0100

    write data as float into influxdb

--- sensor-opc-server/Database/TimeSeriesDataBase.cs
+++ sensor-opc-server/Database/TimeSeriesDataBase.cs
@@ -10,21 +10,22 @@ namespace sensor_opc_server.Database
 
     public class TimeSeriesDataBase : ITimeSeriesDataBase
     {
-        private readonly string Token = "EDGBhfzL5Wahqr-_v7OOQG1yu53JJXPOkfS0YzLLlQDMxbOAd5phc5PH60LquSuQQaCstacljgrsO_B_O-sMTQ==";
         private readonly HttpClient _client;
         private readonly string _writeUrl;
         private readonly ILogger _logger;
+        private ulong _counter;
 
         public TimeSeriesDataBase(ILogger logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _client = new HttpClient(); 
+            _client = new HttpClient();
+            _counter = 0; 
             
-            var ip = "172.28.176.1"; //"localhost";
-            var org = "air";
-            var bucket = "telemetry";
-            var precision = "ms";
-            _writeUrl = $"https://{ip}:8086/api/v2/write?org={org}&bucket={bucket}&precision={precision}";
+            var ip = "localhost";
+            var dbName = "db0";
+            _writeUrl = $"http://{ip}:8086/write?db={dbName}";
+
+            //todo create db if not exist
         }
 
         void IDisposable.Dispose()
@@ -38,17 +39,25 @@ namespace sensor_opc_server.Database
 
             try 
             {
-                var timestamp = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+                var timestamp = new DateTimeOffset(DateTime.Now).ToUnixTimeMilliseconds();
                 using(var request = new HttpRequestMessage())
                 {
                     request.RequestUri = new Uri(_writeUrl);
                     request.Method = HttpMethod.Post;
-                    request.Headers.Add("Authorization", $"Token {Token}");
-                    //request.Headers.Authorization = new AuthenticationHeaderValue("Basic", Token);
-                    request.Content = new StringContent($"{model.Identifier},sensor=MQ135 value=\"{model.Value}\" {timestamp}");
+                    var content = $"{model.Identifier},sensor=MQ135 value={model.Value.ToString("E5")} {timestamp}";
+                    _logger.Verbose("TimeSeriesDB {Request}", content);
+                    request.Content = new StringContent(content);
                     using (var response = await _client.SendAsync(request))
                     {
-                        _logger.Information("Send data to InfluxDB, {StatusCode} {Content}", response.StatusCode, response.Content);
+                        _counter++;
+                        if (!response.IsSuccessStatusCode) {
+                            _logger.Warning("Send data to InfluxDB, {StatusCode} {Content}", response.StatusCode, response.Content);
+                        }
+                    }
+
+                    if ((_counter % 50) == 0)
+                    {
+                        _logger.Information("{TotalNumber} measurements added to Timeseries database", _counter);
                     }
                 }
             }
